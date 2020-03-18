fe084b329df8bb54db5f0f621f49b93a49a4b684
MrNobad <1nobad@gmail.com>
2020-03-18 20:17:48 +0000

    Fixed nav bar flex and added pics

--- decor_web.js
+++ decor_web.js
@@ -1,3 +1,14 @@
 require('bootstrap')
+require('style.css')
 
 new WOW().init();
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
