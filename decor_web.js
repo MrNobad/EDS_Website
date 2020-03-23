4152047a6e82d8641ce75a1cf6b63a5519552c4e
MrNobad <1nobad@gmail.com>
2020-03-23 13:50:57 +0000

    Nav bar semi-fixed

--- decor_web.js
+++ decor_web.js
@@ -4,11 +4,11 @@ require('style.css')
 new WOW().init();
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
